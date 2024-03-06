4c6cb208d2dc7de1881405a66e3df9db005a6ec8
李俊峰 <leedaisen@hotmail.com>
2024-03-06 14:13:39 +0800

    修改配置

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+    APP_NAME: "LeeLaa-Admin",
+    description: 'Vue 3 + TypeScript + Vite',
+    siteLanguage: 'en',
+}
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
+    vueJsx()
   ],
   resolve: {
     alias: {
