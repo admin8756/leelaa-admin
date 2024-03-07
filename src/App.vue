b1545418e25cb68fb315007765bc57ad67eb106d
李俊峰 <leedaisen@hotmail.com>
2024-03-07 17:58:44 +0800

    fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <script setup>
-
 </script>
 
 <template>
-    <header>
-      <nav>
-          <RouterLink to="/" exact>首页</RouterLink>
-          <RouterLink to="/about">关于</RouterLink>
-          <RouterLink to="/login">登录</RouterLink>
-        </nav>
-    </header>
     <RouterView />
 </template>
