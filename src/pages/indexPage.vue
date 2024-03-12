49c695750dbab41de157e2f0dd7843e105cf7aa8
李俊峰 <leedaisen@hotmail.com>
2024-03-12 17:48:29 +0800

    测试主题切换

--- src/pages/indexPage.vue
+++ src/pages/indexPage.vue
@@ -1,5 +1,29 @@
 <script setup></script>
 <template>
   <div class="text-red-600 p-3 text-2xl">Index Page</div>
-  <button type="button">test</button>
+  <div class="join join-vertical">
+    <input
+      type="radio"
+      name="theme-buttons"
+      class="btn theme-controller join-item"
+      aria-label="Default"
+      value="default"
+    />
+    <input type="radio" name="theme-buttons" class="btn theme-controller join-item" aria-label="Retro" value="retro" />
+    <input
+      type="radio"
+      name="theme-buttons"
+      class="btn theme-controller join-item"
+      aria-label="Cyberpunk"
+      value="cyberpunk"
+    />
+    <input
+      type="radio"
+      name="theme-buttons"
+      class="btn theme-controller join-item"
+      aria-label="Valentine"
+      value="valentine"
+    />
+    <input type="radio" name="theme-buttons" class="btn theme-controller join-item" aria-label="Aqua" value="aqua" />
+  </div>
 </template>
