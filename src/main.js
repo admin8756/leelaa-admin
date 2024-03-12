c091bdaed9b42a48498650a6c14176e04c42dc7e
李俊峰 <leedaisen@hotmail.com>
2024-03-12 17:57:08 +0800

    优化打包体积

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import '@/assets/main.css';
 import '@/assets/nprogress.css';
 import 'tailwindcss/tailwind.css';
-import 'daisyui/dist/full.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
