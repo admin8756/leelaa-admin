cd537d871da5f29f4b5236bdde6d91ab12d2df84
李俊峰 <leedaisen@hotmail.com>
2024-04-17 17:25:27 +0800

    优化页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../pages/indexPage.vue';
+
+import { name } from '../../package.json';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,25 +8,36 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      meta: {
+        title: '首页',
+      },
+      component: () => import('../pages/indexPage.vue'),
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      meta: {
+        title: '关于',
+      },
       component: () => import('../pages/aboutPage.vue'),
     },
     {
       path: '/login',
       name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      meta: {
+        title: '登录',
+      },
       component: () => import('../pages/loginPage.vue'),
     },
   ],
+});
+// 设置路由守卫，在页面加载完成的时候，切换标题
+router.beforeEach((to, from, next) => {
+  if(!to.meta?.title){
+    console.error(`${to.name}页，标题未定义`)
+  }
+  document.title = `${to.meta.title} - ${name}`;
+  next();
 });
 
 export default router;
