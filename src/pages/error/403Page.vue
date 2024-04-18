b1d43d619ee4ff7dfd374ee93820f7b04c821124
小生 <leedaisen@hotmail.com>
2024-04-18 21:12:45 +0800

    优化404 和403页面

--- src/pages/error/403Page.vue
+++ src/pages/error/403Page.vue
@@ -1,15 +1,17 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+const goHome = () => {
+  useRouter().push('/');
+};
+</script>
 <template>
-  <div
-    class="hero min-h-screen"
-    style="background-image: url(https://api.luvying.com/acgimg)"
-  >
+  <div class="hero min-h-screen" style="background-image: url(https://api.luvying.com/acgimg)">
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
       <div class="max-w-md">
         <h1 class="mb-5 text-5xl font-bold">403 访问被拒绝</h1>
         <p class="mb-5">您的权限不足，无法访问该页面</p>
-        <button class="btn btn-primary">回到首页</button>
+        <button class="btn btn-primary" @click="goHome">回到首页</button>
       </div>
     </div>
   </div>
--- src/pages/error/404Page.vue
+++ src/pages/error/404Page.vue
@@ -1,4 +1,9 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+const goHome = () => {
+  useRouter().push('/');
+};
+</script>
 <template>
   <div class="hero min-h-screen" style="background-image: url(https://api.luvying.com/acgimg)">
     <div class="hero-overlay bg-opacity-60"></div>
@@ -6,7 +11,7 @@
       <div class="max-w-md">
         <h1 class="mb-5 text-5xl font-bold">404 页面未找到</h1>
         <p class="mb-5">您流浪到了未知的页面，请检查您输入的网址是否正确。</p>
-        <button class="btn btn-primary">回到首页</button>
+        <button class="btn btn-primary" @click="goHome">回到首页</button>
       </div>
     </div>
   </div>
