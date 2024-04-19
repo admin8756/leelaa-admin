be3cec4497d1cb713c9c863af88f302500cfb531
小生 <leedaisen@hotmail.com>
2024-04-20 00:18:05 +0800

    fix bug

--- src/pages/aboutPage.vue
+++ src/pages/aboutPage.vue
@@ -5,9 +5,8 @@ import { dependencies, devDependencies } from '../../package.json';
 <template>
   <div class="overflow-x-auto p-4 flex mt-4">
     <div class="card w-96 bg-base-100 shadow-xl mr-4">
-    <h1 class="m-2">运行依赖</h1>
+      <h1 class="m-2">运行依赖</h1>
       <table class="table">
-        <!-- head -->
         <thead>
           <tr>
             <th>依赖库</th>
@@ -15,7 +14,6 @@ import { dependencies, devDependencies } from '../../package.json';
           </tr>
         </thead>
         <tbody>
-          <!-- row 1 -->
           <tr v-for="(item, index) in dependencies" :key="index">
             <th>{{ index }}</th>
             <td>{{ item }}</td>
@@ -25,9 +23,8 @@ import { dependencies, devDependencies } from '../../package.json';
     </div>
 
     <div class="card w-96 bg-base-100 shadow-xl">
-    <h1 class="m-2">开发依赖</h1>
+      <h1 class="m-2">开发依赖</h1>
       <table class="table">
-        <!-- head -->
         <thead>
           <tr>
             <th>依赖库</th>
@@ -35,7 +32,6 @@ import { dependencies, devDependencies } from '../../package.json';
           </tr>
         </thead>
         <tbody>
-          <!-- row 1 -->
           <tr v-for="(item, index) in devDependencies" :key="index">
             <th>{{ index }}</th>
             <td>{{ item }}</td>
@@ -46,6 +42,4 @@ import { dependencies, devDependencies } from '../../package.json';
   </div>
 </template>
 
-<style>
-/* 在这里添加 Tailwind CSS 样式 */
-</style>
+<style scoped></style>
