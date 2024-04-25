1f415518cb90a26ee7754bad3cb5fe8a439b18a2
leelaa <leedaisen@hotmail.com>
2024-04-25 17:04:16 +0800

    fix bug

--- src/layouts/components/navbar/modules/LanguageBtn.vue
+++ src/layouts/components/navbar/modules/LanguageBtn.vue
@@ -25,15 +25,15 @@ const setLanguage = (lang) => {
       class="dropdown-content bg-base-200 text-base-content rounded-box top-px mt-16 max-h-[calc(100vh-10rem)] w-56 overflow-y-auto border border-white/5 shadow-2xl outline outline-1 outline-black/5"
     >
       <ul class="menu menu-sm gap-1">
-        <li v-for="(locale, index) in locList" :key="index">
-          <button class="btn glass flex justify-between w-full" @click="setLanguage(locale.loc)">
+        <li v-for="(item, index) in locList" :key="index">
+          <button class="btn glass flex justify-between w-full" @click="setLanguage(item.loc)">
             <span
               class="badge badge-sm badge-outline !pl-1.5 !pr-1 pt-px font-mono !text-[.6rem] font-bold tracking-widest opacity-50"
-              :class="{ 'badge-primary': index === locale }"
+              :class="{ 'badge-primary': item.loc === locale }"
             >
-              {{ locale.code }}
+              {{ item.code }}
             </span>
-            <span class="font-[sans-serif]">{{ locale.lang }}</span>
+            <span class="font-[sans-serif]">{{ item.lang }}</span>
           </button>
         </li>
       </ul>
