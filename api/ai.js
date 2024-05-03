d2e1e57c81b00be9cd73f260d1989acab57568bc
leelaa <leedaisen@hotmail.com>
2024-05-03 23:48:20 +0800

    优化接口

--- api/ai.js
+++ api/ai.js
@@ -1,7 +1,13 @@
 export default function handler(request, response) {
-  let { text } = request.query
+  let { text } = request.query;
   text = text.replace(/\?/g, '！');
   text = text.replace('吗', '');
-  text = text.replace('你','我')
-  response.status(200).json(text);
+  if (text.includes('你')) {
+    text = text.replace('你', '我');
+    return response.status(200).json(text);
+  }
+  if (text.includes('我')) {
+    text = text.replace('我', '你');
+    response.status(200).json(text);
+  }
 }
