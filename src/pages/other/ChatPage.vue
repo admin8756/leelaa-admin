70d88ecac628412e7d4158c7557c6b46952bc27b
leelaa <leedaisen@hotmail.com>
2024-05-04 22:28:39 +0800

    优化聊天界面

--- src/pages/other/ChatPage.vue
+++ src/pages/other/ChatPage.vue
@@ -3,7 +3,7 @@ import { ref, onMounted } from "vue";
 import { chat } from "@/api/index";
 const msgList = ref([]);
 const message = ref("");
-
+const isSending = ref(false);
 const baseMsg = (self, msg) => {
   return {
     type: "text",
@@ -12,7 +12,7 @@ const baseMsg = (self, msg) => {
     state: true,
     time: new Date().toLocaleString(),
     avatar: "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "我",
+    name: self ? "天道" : "我",
   };
 };
 onMounted(() => {
@@ -22,6 +22,7 @@ onMounted(() => {
 
 const sendMsg = (msg) => {
   msg = msg.trim();
+  isSending.value = true;
   if (msg.length > 0) {
     msgList.value.push(baseMsg(false, msg));
     chat(msg)
@@ -32,6 +33,7 @@ const sendMsg = (msg) => {
         console.log(err);
       })
       .finally(() => {
+        isSending.value = false;
         message.value = "";
       });
   } else {
@@ -41,29 +43,57 @@ const sendMsg = (msg) => {
 </script>
 <template>
   <div class="flex min-h-[90vh]">
-    <div class="friend flex-1 bg-white"></div>
-    <div class="chat-view p-3 relative" style="flex: 5">
-      <div
-        class="chat"
-        v-for="(item, index) in msgList"
-        :class="item.self ? 'chat-start' : 'chat-end'"
-        :key="index"
-      >
-        <div class="chat-image avatar">
-          <div class="w-10 rounded-full">
-            <img alt="Tailwind CSS chat bubble component" :src="item.avatar" />
+    <!-- 好友列表 -->
+    <div class="friend flex-1 bg-white overflow-y-auto">
+      <div class="flex p-3 border-b-2" v-for="(item, index) in msgList" :key="index">
+        <div class="avatar">
+          <div class="w-16 rounded">
+            <img :src="item.avatar" />
           </div>
         </div>
-        <div class="chat-header">
-          {{ item.name }}
-          <time class="text-xs opacity-50">{{ item.time }}</time>
+        <div class="ml-2">
+          <div class="content font-bold">{{ item.name }}</div>
+          <div class="mt-2 text-sm text-gray-500 text-ellipsis">
+            {{ item.content }}
+          </div>
         </div>
-        <div class="chat-bubble">{{ item.content }}</div>
-        <div class="chat-footer opacity-50">{{ (item.state && "已送达") || "未读" }}</div>
+      </div>
+    </div>
+    <!-- 聊天窗口 -->
+    <div class="chat-view p-3 relative" style="flex: 5">
+      <div class="max-h-[90vh] overflow-y-auto pb-20">
+        <div
+          class="chat"
+          v-for="(item, index) in msgList"
+          :class="item.self ? 'chat-start' : 'chat-end'"
+          :key="index"
+        >
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" :src="item.avatar" />
+            </div>
+          </div>
+          <div class="chat-header">
+            {{ item.name }}
+            <time class="text-xs opacity-50">{{ item.time }}</time>
+          </div>
+          <div class="chat-bubble">
+            {{ item.content }}
+          </div>
+          <div class="chat-footer opacity-50">
+            {{ (item.state && "已送达") || "未读" }}
+          </div>
+        </div>
       </div>
       <div class="absolute bottom-0 left-3 right-3 w-full">
         <div class="flex justify-between">
-          <input type="text" class="w-full mr-3 p-3" v-model="message" />
+          <input
+            type="text"
+            class="w-full mr-3 p-3"
+            :disabled="isSending"
+            v-model="message"
+            @keyup.enter="sendMsg(message)"
+          />
           <button class="btn btn-primary" @click="sendMsg(message)">发送</button>
         </div>
       </div>
